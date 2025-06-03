7870db5d93f57816eefe462da6b1d5f79fa7cf31
Sparerow1 <yc01jun6@gmail.com>
2025-06-03 12:57:14 -0400

    setting up socket server

--- backend/src/index.js
+++ backend/src/index.js
@@ -6,3 +6,19 @@ const port = process.env.PORT || 3000;
 const { WebSocketServer } = require('ws')
 const sockserver = new WebSocketServer({ port: 443 })
 
+sockserver.on('connection', (ws) => {
+console.log('New client connected');
+
+ws.on('message', (message) => {
+  console.log(`Received message: ${message}`);
+  // Echo the message back to the client
+  ws.send(`Server received: ${message}`);
+});
+ws.on('close', () => {
+  console.log('Client disconnected');
+});
+ws.on('error', (error) => {
+    console.error(`WebSocket error: ${error}`);
+});
+}
+);
