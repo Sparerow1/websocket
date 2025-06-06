2b9a4967b59f0829e09c645c5542d40c414037ce
Sparerow1 <yc01jun6@gmail.com>
2025-06-06 10:54:07 -0400

    serving html

--- backend/src/app.js
+++ backend/src/app.js
@@ -2,20 +2,20 @@ const express = require('express');
 const cookieParser = require('cookie-parser');
 const routes = require('./routes');
 const { environment } = require('./config');
+const path = require('path');
 const isProduction = environment === 'production';
 
 const app = express();
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
-app.use(express.static('public'));
+
+app.use(express.static(path.join(__dirname, '../public')));
 
 app.use(routes); // Register routes from the routes module
-app.use((req, res) => {
-  res.status(404).send('Not Found');
-});
 
-// Catch unhandled requests and forward to error handler.
+//  Catch unhandled requests and forward to error handler.
 app.use((_req, _res, next) => {
   const err = new Error("The requested resource couldn't be found.");
   err.title = "Resource Not Found";
@@ -47,25 +47,3 @@ app.use((err, _req, res, _next) => {
 
 //export app
 module.exports = app;
-// const express = require('express')
-// const webserver = express()
-//  .use((req, res) =>
-//    res.sendFile('public/websocket-client.html', { root: __dirname })
-//  )
-//  .listen(3000, () => console.log(`Listening on ${3000}`))
-// const { WebSocketServer } = require('ws')
-// const sockserver = new WebSocketServer({ port: 8080 })
-// sockserver.on('connection', ws => {
-//  console.log('New client connected!')
-//  ws.send('connection established')
-//  ws.on('close', () => console.log('Client has disconnected!'))
-//  ws.on('message', data => {
-//    sockserver.clients.forEach(client => {
-//      console.log(`distributing message: ${data}`)
-//      client.send(`${data}`)
-//    })
-//  })
-//  ws.onerror = function () {
-//    console.log('websocket error')
-//  }
-// })
