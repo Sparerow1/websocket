ab45e4d48abd757be2c66ad743ca8b61b0f4ab9a
Sparerow1 <yc01jun6@gmail.com>
2025-06-05 22:19:44 -0400

    setting up websocket functionality

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,35 @@
 const app = require('./src/app');
 const { port } = require('./src/config');
+const { WebSocketServer } = require('ws');
+const http = require('http');
+const server = http.createServer(app);
+const wss = new WebSocketServer({ server });
+
+wss.on('connection', (ws, req) => {  
+  console.log('New Client Connected');
+
+  // send a welcome message to the newly connected client
+  ws.send(JSON.stringify({  
+    type: 'connection',  
+    message: 'Welcome to the WebSocket server!',
+    timestamp: new Date().toISOString()
+  }));  
+
+  // handle incoming messages from clients
+  ws.on('message', (data) => {
+    try { 
+
+    } catch (error) {
+      console.error('Error processing message:', error);
+      ws.send(JSON.stringify({  
+        type: 'error',  
+        message: 'An error occurred while processing your message.',
+        timestamp: new Date().toISOString()
+      }));
+    }
+  })
+  
+});
 
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
